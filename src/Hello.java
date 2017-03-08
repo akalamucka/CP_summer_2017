d6e28c5a688ac8787348ab8ecc43eccea18a4ff9
Agata Kalamucka <agata.kalamucka@gmail.com>
2017-03-08 18:45:16 +0100

    first commitment

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,32 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int a;
+        a = 5;
+        int b = ++a;
+// moze byc a++ i to bedzie co innego znaczylo
+        // b = b + 5
+        int c = 11 % 4;
+
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+        System.out.println("c=" + c);
+
+        double d1 = 45.5;
+        double d2 = 5.2;
+        double d3 = d1 / d2;
+
+        System.out.println(d3);
+
+        Float f1 = new Double(d3).floatValue();
+        f1 = new Integer(b).floatValue();
+
+        System.out.println(f1);
+
+        Integer i1 = f1.intValue();
+
+        System.out.println(i1);
     }
 }
 
---
+++ src/TextOper.java
@@ -0,0 +1,40 @@
+import javax.xml.bind.SchemaOutputResolver;
+
+/**
+ * Created by ak63575 on 3/8/2017.
+ */
+public class TextOper {
+
+    public static void main(String[] args) {
+        String s1 = "abc";
+        String s2 = "def";
+        String s3 = s1 + s2;
+
+        int posBC = s3.indexOf("ab");
+
+        String s4 = s3.substring(2, 4);
+
+        s4 = s3.replace("ab", "AB");
+        s4 = s4.toUpperCase();
+        int s4len = s4.length();
+
+        System.out.println(s4);
+        System.out.println(s3);
+        System.out.println(posBC);
+        System.out.println(s4len);
+
+        char c1 = s4.charAt(4);
+
+        System.out.println(c1);
+
+        if (s4.contains("F")) {
+            System.out.println("s4 contains F");
+        } else {
+            System.out.println("s4 doesn't contain F");
+        }
+
+        if (s4.contains("F") && (s4.length() < 7)) {
+            System.out.println("s4 contains F");
+        } else {
+            System.out.println("s4 doesn't contain F");
+        }
